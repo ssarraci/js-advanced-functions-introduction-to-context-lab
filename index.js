1ce54d1ab1aa32dc09341794118d4d1a2c1fcc64
Storm <storm.sarracino01@gmail.com>
2021-07-31 12:41:38 -0500

    commit tree 4053b70d2b8822cb850aa350e6fd6826518a8bd6

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Your code here
+function createEmployeeRecord(x) {
+   return {
+      firstName: x[0],
+      familyName: x[1],
+      title: x[2],
+      payPerHour: x[3],
+      timeInEvents: [],
+      timeOutEvents: []
+   }
+}
+
+let createEmployeeRecords = (dataOfEmployees) => {
+   return dataOfEmployees.map(function(row){
+      return createEmployeeRecord(row)
+   })
+}
+
+function createTimeInEvent(employee, timeStamp) {
+   let [date,hour] = timeStamp.split(' ')
+
+   employee.timeInEvents.push({
+      type: "TimeIn",
+      date: date,
+      hour: parseInt(hour, 10),
+   })
+   return employee
+}
+
+function createTimeOutEvent(employee, timeStamp) {
+   let [date,hour] = timeStamp.split(' ')
+   
+   employee.timeOutEvents.push({
+      type: "TimeOut",
+      date: date,
+      hour: parseInt(hour, 10),
+   })
+   return employee
+}
